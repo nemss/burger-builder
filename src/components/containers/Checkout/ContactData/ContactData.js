5e9f072d1e64b0e8a03bc8bda4f9d08830fbcecd
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-13 10:18:14 +0200

    added validation feedback

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -148,6 +148,8 @@ export default class ContactData extends Component {
 						elementType={formElement.config.elementType}
 						elementConfig={formElement.config.elementConfig}
 						value={formElement.config.value}
+						invalid={!formElement.config.valid}
+						shouldValidate={formElement.config.validation}
 						changed={(event) => this.inputChangeHandler(event, formElement.id)}/>
 				))}
 				<Button btnType='Success'>ORDER</Button>
