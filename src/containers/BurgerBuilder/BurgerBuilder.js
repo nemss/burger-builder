97c7df828f80061c17339e8c428c95b564eb993b
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-17 16:07:22 +0200

    initializing ingredients in the burgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,6 +15,11 @@ class BurgerBuilder extends Component {
 		purchasing: false
 	};
 
+	componentDidMount() {
+		this.props.onInitIngredients();
+	}
+
+
 	updatePurchaseState(ingredients) {
 		const sum = Object.keys(ingredients)
 			.map(igKey => ingredients[igKey])
@@ -42,7 +47,7 @@ class BurgerBuilder extends Component {
 			disableInfo[key] = disableInfo[key] <= 0
 		}
 		let orderSummary = null;
-		let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner/>;
+		let burger = this.props.error ? <p>Ingredients can't be loaded</p> : <Spinner/>;
 
 		if (this.props.ings) {
 			burger = (
@@ -78,14 +83,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
 	return {
 		ings: state.ingredients,
-		price: state.totalPrice
+		price: state.totalPrice,
+		error: state.error
 	}
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
 		onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-		onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+		onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+		onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
 	}
 };
 
