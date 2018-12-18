36f08310ea6d5ef20bf5dda9a4ac9254ceec8c62
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-18 10:20:25 +0200

    connecting contact data container & actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner.css';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actons from '../../../store/actions/index';
 
 class ContactData extends Component {
 	state = {
@@ -106,6 +108,8 @@ class ContactData extends Component {
 			price: this.props.price,
 			orderData: formData
 		};
+
+		this.props.onOrderBurger(order)
 	};
 
 	checkValidity(value, rules) {
@@ -191,4 +195,10 @@ const mapStateToProps = state => {
 	}
 };
 
-export default connect(mapStateToProps)(ContactData)
+const mapDispatchToProps = dispatch => {
+	return {
+		onOrderBurger: (orderData) => dispatch(actons.purchaseBurgerStart(orderData))
+	}
+};
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -21,7 +21,7 @@ export const purchaseBurgerStart = (orderData) => {
 		axios.post('/orders.json', orderData)
 			.then(response => {
 				console.log(response.data);
-				dispatch(purchaseBurgerSuccess(response.data, orderData);
+				dispatch(purchaseBurgerSuccess(response.data, orderData))
 			})
 			.catch(error => {
 				dispatch(purchaseBurgerFail(error))
