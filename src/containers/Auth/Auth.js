8ac6d8884caf2af3d0926bb9d9e085ffbc348a42
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-20 11:38:26 +0200

    persistent auth state with localStorage

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -45,7 +45,9 @@ class Auth extends Component {
 	};
 
 	componentDidMount() {
-
+		if(!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+			this.props.onSetAuthRedirectPath()
+		}
 	}
 
 
@@ -171,7 +173,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
 	return {
 		onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
-		onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath())
+		onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
 	}
 };
 
