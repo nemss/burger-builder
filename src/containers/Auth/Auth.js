0071805d27398529e288df4d297d0531041116f0
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-20 11:04:40 +0200

    redirecting the user

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner'
 import classes from './Auth.css';
-import * as actiosn from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 
 class Auth extends Component {
@@ -43,6 +43,11 @@ class Auth extends Component {
 		},
 		isSignup: true
 	};
+
+	componentDidMount() {
+
+	}
+
 
 	checkValidity(value, rules) {
 		let isValid = true;
@@ -134,7 +139,7 @@ class Auth extends Component {
 		}
 		let authRedirect = null;
 		if (this.props.isAuthenticated) {
-			authRedirect = <Redirect to='/'/>
+			authRedirect = <Redirect to={this.props.authRedirectPath}/>
 		}
 
 		return (
@@ -157,13 +162,16 @@ const mapStateToProps = state => {
 	return {
 		loading: state.auth.loading,
 		error: state.auth.error,
-		isAuthenticated: state.auth.token !== null
+		isAuthenticated: state.auth.token !== null,
+		buildingBurger: state.burgerBuilder.building,
+		authRedirectPath: state.auth.authRedirectPath
 	}
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onAuth: (email, password, isSignup) => dispatch(actiosn.auth(email, password, isSignup))
+		onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+		onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath())
 	}
 };
 
