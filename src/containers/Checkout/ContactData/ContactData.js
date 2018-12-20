333986923e2b70fce71b7b42a9ec62988bed6165
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-20 14:39:20 +0200

    sharing the validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import {updateObject} from "../../../shared/utility";
+import {updateObject, checkValidity} from "../../../shared/utility";
 
 class ContactData extends Component {
 	state = {
@@ -112,31 +112,10 @@ class ContactData extends Component {
 		this.props.onOrderBurger(order, this.props.token)
 	};
 
-	checkValidity(value, rules) {
-		let isValid = true;
-		if (!rules) {
-			return true;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-
-		if (rules.minlength) {
-			isValid = value.length >= rules.minlength && isValid;
-		}
-
-		if (rules.maxlength) {
-			isValid = value.length <= rules.maxlength && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangeHandler = (event, inputIdentifier) => {
 		const updateFormElement = updateObject(this.state.orderForm[inputIdentifier], {
 		value: event.target.value,
-		valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+		valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
 		touched:true
 	});
 
