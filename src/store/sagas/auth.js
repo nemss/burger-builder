f337e6116ebf592f63d723d18a03baca67d8d323
Valentin Parvanov <valentinparvanov@outlook.com>
2018-12-21 13:46:56 +0200

    moving the burger builder side effects into a saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -3,9 +3,6 @@ import {put} from 'redux-saga/effects'
 
 import * as actions from '../actions/index';
 import axios from "axios";
-import {logout} from "../actions/index";
-import {authSuccess} from "../actions/index";
-import {checkAuthTimeout} from "../actions/index";
 
 export function* logoutSaga(action) {
 	yield localStorage.removeItem('token');
